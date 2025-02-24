80661cf92b6a867aa7530d54286d68dd62610e4b
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-02-24 15:32:09 +0700

    learnt new things

--- helloWorld.c
+++ helloWorld.c
@@ -5,3 +5,5 @@ void main() {
 }
 
 // a little expermients
+// learnt new thing, I can actually set up git using personal access token and 
+// automate using git remote set-url origin https://<username>:<token>@github.com/<username>/<repository>.git
